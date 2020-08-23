be831e967001dfd3629b64e823a094a2545170ef
kuro <black_assassin1991@yahoo.com>
2020-08-24 02:44:12 +0430

    added script for linux crontab

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 . /home/kuro/.bashrc
-echo "[`date`] Cron task started" >> sc.log
-result=`ps aux | grep -i "script.sh" | grep -v "grep" | wc -l`
+echo "[$(date)] Cron task started" >> sc.log
+result=$(ps aux | grep -i "script.sh" | grep -v "grep" | wc -l)
 echo $result
 if [ $result -ge 3 ]
    then
-        echo "[${date}] script is already running" >> sc.log
-        echo "[${date}] exiting" >> sc.log
+        echo "[$(date)}] script is already running" >> sc.log
+        echo "[$(date)] exiting" >> sc.log
    else
-        echo "[`date`] starting recording script" >> sc.log
+        echo "[$(date)] starting recording script" >> sc.log
         cd /home/kuro/bbb-recorder                                                                                                                                                                                                                                                        /usr/local/bin/node export.js --rebuild > app.log 2>&1                                                                                                                                                                                                                    f
 fi
