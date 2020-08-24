c7b1a4750ffbf455b67e3cb79a31e0bd74e9381c
kuro <black_assassin1991@yahoo.com>
2020-08-24 18:42:04 +0430

    added control over process

---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+. /home/kuro/.bashrc
+echo "[$(date)] Cron task started" 2>>sc.log
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+else
+  echo ".env file cannot be found" 2>>sc.log
+  exit 1
+fi
+lockdir=""
+for ((i = 1; i <= SPAWNS; i++)); do
+  templockdir="${baseLockDir}/bbb-recorder-lockdir-${i}"
+  if [[ -d $templockdir ]]; then
+    continue
+  else
+    lockdir=$templockdir
+    mkdir $lockdir
+    echo "[$(date)] ${lockdir} created" 2>>sc.log
+    break
+  fi
+done
+trap 'rm -rf $lockdir' EXIT INT TERM
+
+if [ -z "${lockdir}" ]; then
+  echo "All dir locked, stopping script" 2>>sc.log
+  exit 1
+else
+  echo "[$(date)] starting recording script" 2>>sc.log
+  cd /home/kuro/bbb-recorder
+  /usr/local/bin/node export.js --lockdir "${lockdir}" --rebuild >app.log 2>&1
+fi
+
+# take pains to remove lock directory when script terminates
+
+# Create a file with current PID to indicate that process is running.
+#echo $$ 2>>"${lockdir}/$$"
---
+++ stop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+. /home/kuro/.bashrc
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+else
+  echo ".env file cannot be found" 2>>sc.log
+  exit 1
+fi
+echo "[$(date)] Cron task started" 2>>sc.log
+
+for ((i = 1; i <= SPAWNS; i++)); do
+  lockdir="${baseLockDir}/bbb-recorder-lockdir-${i}"
+  if [[ -d $lockdir ]]; then
+    for entry in "${lockdir}"/*; do
+      echo $(basename "${entry}")
+      pid=$(basename "${entry}")
+      echo "entry: ${entry}"
+      echo "pid: ${pid}"
+      sudo kill -QUIT "${pid}" &
+      echo "stoped"
+    done
+  fi
+done
